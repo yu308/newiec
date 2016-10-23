91169c3ff5a65ede59e54eee726f7e50c90e1d3a
yu_arch <604779367@qq.com>
2016-10-23 09:56:57 +0800

    添加注释

--- Core/iec_sys.c
+++ Core/iec_sys.c
@@ -15,6 +15,14 @@ void iec_main_thread_entry(void *param);
 #define MAX_EVENT_COUNT			(5)
 
 
+/** 
+ * 查找当前系统中某个链路
+ * 
+ * @param info 系统信息实例
+ * @param name 链路名称
+ * 
+ * @return 链路ID  0 未找到
+ */
 static int iec_sys_find_link(struct sys_info *info,char *name)
 {
   int i=0;
@@ -30,6 +38,14 @@ static int iec_sys_find_link(struct sys_info *info,char *name)
   return 0;
 }
 
+/** 
+ *  添加一个link
+ * 
+ * @param info 系统信息实例 
+ * @param link_id 链路ID
+ * 
+ * @return  1 添加成功  0 当前系统支持链路已满
+ */
 static int iec_sys_add_link(struct sys_info *info,void *link_id)
 {
   int i=0;
@@ -47,6 +63,12 @@ static int iec_sys_add_link(struct sys_info *info,void *link_id)
   return 0;
 }
 
+/** 
+ * 创建链路实例事件处理函数
+ * 
+ * @param info 系统信息实例
+ * @param evt  事件 包含链路创建时所需信息
+ */
 static void iec_sys_evt_create_link_handle(struct sys_info *info,struct iec_event *evt)
 {
   int sub_evt=evt->evt_sub_type;
@@ -59,7 +81,7 @@ static void iec_sys_evt_create_link_handle(struct sys_info *info,struct iec_even
     case EVT_SUB_SYS_LINK_SERIAL:
       link_p=evt->sub_msg;
       serial_link=serial_link_create(link_p->name,link_p->link_addr,link_p->link_addr_len,link_p->link_dir);
-      if(iec_sys_add_link(info, serial_link)==0)
+      if(iec_sys_add_link(info, serial_link)==0) /*系统支持链路缓存已满  */
         {
           serial_link_del(serial_link);
           return;
@@ -71,6 +93,12 @@ static void iec_sys_evt_create_link_handle(struct sys_info *info,struct iec_even
     }
 }
 
+/** 
+ * 系统启动事件处理函数
+ * 
+ * @param info 系统信息实例
+ * @param evt 事件  包含需启动链路或APP任务标识
+ */
 static void iec_sys_evt_start_handle(struct sys_info *info,struct iec_event *evt)
 {
   int sub_evt=evt->evt_sub_type;
@@ -155,6 +183,16 @@ void iec_sys_api_start_sys()
 
 
 /***************** API ************************/
+
+/**
+ * 创建链路实例---用户接口
+ *
+ * @param link_name  链路实例名称
+ * @param link_type  链路类型  1 串行   2 网络eth
+ * @param link_addr  链路地址  网络类型时 表示socket标号
+ * @param link_addr_len  链路地址长度
+ * @param dir   平衡传输或非平衡传输
+ */
 void iec_sys_api_create_link(char *link_name,int link_type,unsigned int link_addr,int link_addr_len,int dir)
 {
   struct iec_event *evt=iec_create_event(0, (int)&gSys_Info, EVT_SYS_CREATE_LINK, 0, 0);
@@ -175,6 +213,12 @@ void iec_sys_api_create_link(char *link_name,int link_type,unsigned int link_add
     }
 }
 
+/** 
+ * 启动链路任务 --- 用户接口
+ * 
+ * @param type  链路类型
+ * @param link_name  对应链路名称
+ */
 void iec_sys_api_start_link(int type,char *link_name)
 {
   int *link_id=rt_malloc(sizeof(int));
